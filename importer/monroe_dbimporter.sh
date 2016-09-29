f7bf0a04cf589f7b1394ef299d27949ebe633af5
Jonas Karlsson <jonas.karlsson@kau.se>
2016-09-29 10:26:41 +0200

    Copy/paste error

--- importer/monroe_dbimporter.sh
+++ importer/monroe_dbimporter.sh
@@ -2,22 +2,29 @@
 export MONROE_DB_USER=USER
 export MONROE_DB_PASSWD=SECRETPASSWORD 
 
+
+mkdir -p /experiments/processed/
+mkdir -p /experiments/failed-retries/
+mkdir -p /experiments/failed/
+
+
 for path in "http" "metadata" "mplane" "ping" "retry"
 do
-        if [[ ! $(screen -r |grep MonroeDB-${path}) ]]
+    if [[ ! $(screen -r |grep MonroeDB-${path}) ]]
+    then
+        mkdir -p /experiments/monroe/${path}
+        if [[ ${path} == "http" ]] 
         then
-                if [[ ${path} == "http" ]] 
-                then
-                        echo "Starting a recursive Importer for ${path}"
-                        screen -dmS MonroeDB-${path} bash -c "python -u monroe_dbimporter.py --indir /experiments/monroe/${path} --failed /experiments/failed/ --processed /experiments/processed/ --authenv  --host=127.0.0.1 --keyspace=monroe --interval=30  --verbosity=1 --concurren$
-                elif [[ ${path} == "retry" ]] 
-                then
-                        echo "Starting a recursive Importer for ${path} (extensive logging)"
-                        screen -dmS MonroeDB-${path} bash -c "python -u monroe_dbimporter.py --indir /experiments/monroe/${path} --failed /experiments/failed/retry/ --processed /experiments/processed/ --authenv  --host=127.0.0.1 --keyspace=monroe --interval=30  --verbosity=2 --con$
-                else
-                        echo "Starting a non recursive Importer for ${path}"
-                        screen -dmS MonroeDB-${path} bash -c "python -u monroe_dbimporter.py --indir /experiments/monroe/${path} --failed /experiments/failed/ --processed /experiments/processed/ --authenv  --host=127.0.0.1 --keyspace=monroe --interval=30  --verbosity=1 --concurren$
-                fi
+            echo "Starting a recursive Importer for ${path}"
+            screen -dmS MonroeDB-${path} bash -c "python -u monroe_dbimporter.py --indir /experiments/monroe/${path} --failed /experiments/failed/ --processed /experiments/processed/ --authenv  --host=127.0.0.1 --keyspace=monroe --interval=30  --verbosity=1 --concurrency=2 --recursive | tee importer_${path}.log"
+        elif [[ ${path} == "retry" ]] 
+        then
+            echo "Starting a recursive Importer for ${path} (extensive logging)"
+            screen -dmS MonroeDB-${path} bash -c "python -u monroe_dbimporter.py --indir /experiments/monroe/${path} --failed /experiments/failed-retries/ --processed /experiments/processed/ --authenv  --host=127.0.0.1 --keyspace=monroe --interval=30  --verbosity=2 --concurrency=1 --recursive | tee importer_${path}.log"
+        else
+            echo "Starting a non recursive Importer for ${path}"
+            screen -dmS MonroeDB-${path} bash -c "python -u monroe_dbimporter.py --indir /experiments/monroe/${path} --failed /experiments/failed/ --processed /experiments/processed/ --authenv  --host=127.0.0.1 --keyspace=monroe --interval=30  --verbosity=1 --concurrency=2 |tee importer_${path}.log"
         fi
+    fi
 done
 screen -r |grep MonroeDB-
