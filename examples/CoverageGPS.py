8de58d1d50d7ec5201b8298d65de5660e00fbf30
Miguel Peón Quirós <mikepeon@imdea.org>
2017-02-09 19:40:02 +0100

    Added example to create a KML mixing GPS positions with modem state. TODO: Make nodeID, startTime and endTime command line parameters. TODO: Add filter by interface name or operator name.

---
+++ examples/CoverageGPS.py
@@ -0,0 +1,286 @@
+#!/usr/bin/python
+
+"""
+ Example tool to convert GPS positions of MONROE nodes in the Cassandra database to KML.
+  https://www.monroe-project.eu
+  Creator: Miguel Peon Quiros, IMDEA Networks Institute
+  mikepeon@imdea.org
+
+ Dependencies: sudo pip install cassandra-driver python-dateutil
+
+ Cassandra driver (Python) documentation: https://datastax.github.io/python-driver/index.html
+"""
+
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from time import struct_time, strftime, gmtime
+from datetime import datetime
+from calendar import timegm
+from dateutil.relativedelta import relativedelta
+from decimal import *
+
+def DumpKML(nodeID, startTime, endTime, entries):
+	fileName = "{}_{}_{}.kml".format(nodeID, startTime, endTime)
+	with open(fileName, "wt") as output:
+                # Write KML file headers.
+                output.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><kml xmlns=\"http://www.opengis.net/kml/2.2\">\n"
+                                "<Document>\n"
+                                "<Style id=\"IconUnknown\">\n"
+				"  <IconStyle>\n"
+				"    <scale>0.5</scale>\n"
+				"    <Icon>\n"
+				"      <href>Mark_Unknown.png</href>\n"
+				"    </Icon>\n"
+				"  </IconStyle>\n"
+				"</Style>\n"
+                                "<Style id=\"IconDisconnected\">\n"
+				"  <IconStyle>\n"
+				"    <scale>0.5</scale>\n"
+				"    <Icon>\n"
+				"      <href>Mark_Disconnected.png</href>\n"
+				"    </Icon>\n"
+				"  </IconStyle>\n"
+				"</Style>\n"
+                                "<Style id=\"IconNoService\">\n"
+				"  <IconStyle>\n"
+				"    <scale>0.5</scale>\n"
+				"    <Icon>\n"
+				"      <href>Mark_NoService.png</href>\n"
+				"    </Icon>\n"
+				"  </IconStyle>\n"
+				"</Style>\n"
+                                "<Style id=\"Icon2G\">\n"
+				"  <IconStyle>\n"
+				"    <scale>0.5</scale>\n"
+				"    <Icon>\n"
+				"      <href>Mark_2G.png</href>\n"
+				"    </Icon>\n"
+				"  </IconStyle>\n"
+				"</Style>\n"
+                                "<Style id=\"Icon3G\">\n"
+				"  <IconStyle>\n"
+				"    <scale>0.5</scale>\n"
+				"    <Icon>\n"
+				"      <href>Mark_3G.png</href>\n"
+				"    </Icon>\n"
+				"  </IconStyle>\n"
+				"</Style>\n"
+                                "<Style id=\"IconLTE\">\n"
+				"  <IconStyle>\n"
+				"    <scale>0.5</scale>\n"
+				"    <Icon>\n"
+				"      <href>Mark_LTE.png</href>\n"
+				"    </Icon>\n"
+				"  </IconStyle>\n"
+				"</Style>\n"
+				"<Folder>\n")
+
+		count = 0
+		for entry in entries:
+			output.write("\n<Placemark>\n"
+				"<description>{}</description>\n"
+				"<styleUrl>{}</styleUrl>\n"
+				"<Point> <coordinates>{},{},{} </coordinates> </Point>\n"
+				"</Placemark>\n".format(
+					entry['description'], entry['iconStyle'], entry['longitude'],
+					entry['latitude'], entry['altitude']))
+			count += 1
+
+                # Write KML file footers.
+		output.write("</Folder>\n</Document>\n</kml>\n")
+
+	print "Dumped {} positions to {}\n".format(count, fileName)
+
+
+###############################################################################
+# Returns a list of GPS positions as read from the query rows.
+def FetchPositions(session, startTime, endTime, nodeID):
+	print "Extracting GPS positions for node {} during interval [{}, {})".format(nodeID, startTime, endTime)
+	
+	session.default_fetch_size = 1000
+	gps = []
+	query = "select nmea, nodeid, timestamp, latitude, longitude, altitude, speed, satellitecount from monroe_meta_device_gps where nodeid='{}' and timestamp >= {} and timestamp < {} order by timestamp asc".format(nodeID, startTime, endTime)
+	print query
+	rows = session.execute(query, timeout = None)
+	count = 0
+	for row in rows:
+		try:
+			if row.nmea.find("GPRMC") != -1:
+				gps.append(row)
+				count += 1
+		except Exception as error:
+			print "Error in row:", row, error
+	print "Read {} GPS positions\n".format(count)
+	return gps
+
+
+###############################################################################
+# Returns a list of modem statuses as read from the query rows.
+def FetchModemStatus(session, startTime, endTime, nodeID, iccids):
+	print "Extracting modem status for node {} during interval [{}, {})".format(nodeID, startTime, endTime)
+	
+	session.default_fetch_size = None
+	modem = []
+	query = "select nodeid,iccid,timestamp,band,devicemode,devicestate,devicesubmode,frequency,interfacename,internalinterface,lac,operator,pci,rscp,rsrp,rsrq,rssi from monroe_meta_device_modem where nodeid='{}' and iccid in ('{}') and timestamp >= {} and timestamp < {} order by timestamp asc".format(nodeID, "','".join(iccids), startTime, endTime)
+	print query
+	rows = session.execute(query, timeout = None)
+	count = 0
+	for row in rows:
+		try:
+			modem.append(row)
+			count += 1
+		except Exception as error:
+			print "Error in row:", row, error
+	print "Read {} modem statuses\n".format(count)
+	return modem
+
+
+###############################################################################
+# Returns the list of iccids associated to the given nodeID
+def FetchNodeICCIDs(session, nodeID):
+	print "Extracting ICCIDs for node {}".format(nodeID)
+	
+	session.default_fetch_size = None
+	query = "select interfaces from devices where nodeid={}".format(nodeID)
+	print query
+	rows = session.execute(query, timeout = None)
+	try:
+		iccids = rows[0].interfaces
+	except Exception as error:
+		print "Error in row:", rows[0], error
+	return iccids
+
+
+###############################################################################
+#  Traverses a list of GPS positions and a list of modem statuses and combines
+# them based on timestamp.
+#  Returns a list of position-modem status.
+def TraverseGPSAndModem(gps, modem):
+	combinedEntries = []
+	lastModem = None
+	lastGPS = None
+	lastTimestamp = 0
+	iModem = 0
+	iGPS = 0
+	while (iModem < len(modem)) and (iGPS < len(gps)):	# TODO Change to or and check for buffer overruns.
+		# Update the last known GPS and modem entries.
+		if (gps[iGPS].timestamp == modem[iModem].timestamp):
+			lastGPS = gps[iGPS]
+			lastModem = modem[iModem]
+			lastTimestamp = lastGPS.timestamp
+			iGPS += 1
+			iModem += 1
+		elif (gps[iGPS].timestamp < modem[iModem].timestamp):
+			lastGPS = gps[iGPS]
+			lastTimestamp = lastGPS.timestamp
+			iGPS += 1
+		else:	# gps[iGPS].timestamp > modem[iModem].timestamp
+			lastModem = modem[iModem]
+			lastTimestamp = lastModem.timestamp
+			iModem += 1
+
+		# Create a new entry with the last known GPS and modem values
+		if (lastGPS != None) and (lastModem != None):
+			iconStyle = "#IconUnknown" if lastModem.devicemode == 0 else "#IconDisconnected" if lastModem.devicemode == 1 else "#IconNoService" if lastModem.devicemode == 2 else "#Icon2G" if lastModem.devicemode == 3 else "#Icon3G" if lastModem.devicemode == 4 else "#IconLTE" if lastModem.devicemode == 5 else "#IconUnknown"
+			try:
+				combinedEntries.append(
+					{'description': 
+						"Node: {}\nTimestamp: {}\nLatitude: {} {}\nLongitude: {} {}\nAltitude: {}\n"
+						"Speed: {} Km/h\nSatellites: {}\n"
+						"Modem mode: {}\nModem submode: {}\n"
+						"ICCID: {}\nBand: {}\nDeviceState: {}\n"
+						"Frequency: {}\nInterfaceName: {}\nInternalInterface: {}\n"
+						"LAC: {}\nOperator: {}\nPCI: {}\nRSCP: {}\nRSRP: {}\nRSSI: {}".format(
+							lastGPS.nodeid, lastGPS.timestamp,						
+                        	                	lastGPS.latitude, 'N' if lastGPS.latitude >= 0.0 else 'S',
+                                	        	lastGPS.longitude, 'E' if lastGPS.longitude >= 0.0 else 'W',
+                                        		lastGPS.altitude, 
+							lastGPS.speed * Decimal('1.852') if lastGPS.speed != None else lastGPS.speed,
+							lastGPS.satellitecount,
+							"Unknown (0)" if lastModem.devicemode == 0 
+								else "Disconnected (1)" if lastModem.devicemode == 1 
+								else "No service (2)" if lastModem.devicemode == 2 
+								else "2G (3)" if lastModem.devicemode == 3 
+								else "3G (4)" if lastModem.devicemode == 4 
+								else "LTE (5)" if lastModem.devicemode == 5 
+								else "None" if lastModem.devicemode == None 
+								else "?? ({})".format(lastModem.devicemode),
+							"Unknown (0)" if lastModem.devicesubmode == 0 
+								else "UMTS (1)" if lastModem.devicesubmode == 1 
+								else "WCDMA (2)" if lastModem.devicesubmode == 2 
+								else "EVDO (3)" if lastModem.devicesubmode == 3 
+								else "HSPA (4)" if lastModem.devicesubmode == 4 
+								else "HSPA+ (5)" if lastModem.devicesubmode == 5 
+								else "DC HSPA (6)" if lastModem.devicesubmode == 6 
+								else "DC HSPA+ (7)" if lastModem.devicesubmode == 7 
+								else "HSDPA (8)" if lastModem.devicesubmode == 8 
+								else "HSUPA (9)" if lastModem.devicesubmode == 9 
+								else "HSDPA+HSUPA (10)" if lastModem.devicesubmode == 10 
+								else "HSDPA+ (11)" if lastModem.devicesubmode == 11 
+								else "HSDPA+HSUPA (12)" if lastModem.devicesubmode == 12 
+								else "DC HSDPA+ (13)" if lastModem.devicesubmode == 13 
+								else "DC HSDPA + HSUPA (14)" if lastModem.devicesubmode == 14 
+								else "None" if lastModem.devicesubmode == None 
+								else "?? ({})".format(lastModem.devicesubmode),
+							lastModem.iccid, lastModem.band, 
+							"Unknown (0)" if lastModem.devicestate == 0 
+								else "Registered (1)" if lastModem.devicestate == 1 
+								else "Unregistered (2)" if lastModem.devicestate == 2 
+								else "Connected (3)" if lastModem.devicestate == 3 
+								else "Disconnected (4)" if lastModem.devicestate == 4 
+								else "None" if lastModem.devicestate == None 
+								else "?? ({})".format(lastModem.devicestate),
+							lastModem.frequency, 
+							lastModem.interfacename, lastModem.internalinterface, lastModem.lac,
+							lastModem.operator.encode('latin-1'), lastModem.pci, lastModem.rscp, lastModem.rsrp,
+							lastModem.rssi),
+					'iconStyle': iconStyle,
+					'longitude': lastGPS.longitude,
+					'latitude': lastGPS.latitude,
+					'altitude': lastGPS.altitude
+					} )
+			except Exception as error:
+				print "-------------------- EXCEPTION: ", error
+				print lastGPS
+				print lastModem
+				print "--------------------"
+	return combinedEntries
+
+
+###############################################################################
+if __name__ == '__main__':
+
+	auth = PlainTextAuthProvider(username = "xxxx", password = "yyyy")
+	cluster = Cluster(contact_points = ['127.0.0.1'], port = 9042, auth_provider = auth)
+	session = None
+	session = cluster.connect("monroe") # Set default keyspace to 'monroe'
+	session.default_timeout = None
+	session.default_fetch_size = 1000
+
+	# Thu Sep 15 2016 14:00:00 GMT+0200 (Romance Daylight Time)
+	startTime = 1486728000  #1486382400
+	endTime = startTime + 3600*72
+	nodeID = 54
+	iccids = FetchNodeICCIDs(session, nodeID)
+	print "Node {} has ICCIDs: {}\n".format(nodeID, iccids)
+        gps = FetchPositions(session, startTime, endTime, nodeID);
+	modem = FetchModemStatus(session, startTime, endTime, nodeID, iccids);
+	combinedEntries = TraverseGPSAndModem(gps, modem)
+	DumpKML(nodeID, startTime, endTime, combinedEntries)
+
+	#print "Total combined entries: {}".format(len(combinedEntries))
+	#for entry in combinedEntries:
+	#	print "{}, {}, {}, {}".format(
+	#		entry['description'], entry['longitude'], entry['latitude'], entry['altitude'])
+	#	print "------------\n"
+	#print "------ GPS ------"
+	#for ii in gps:
+	#	print "{}, {}, {}, {}".format(ii.timestamp, ii.latitude, ii.longitude, ii.altitude)
+	#print "------ MODEM ------"
+	#for ii in modem:
+	#	print "{}, {}, {}, {}, {}, {}".format(ii.timestamp, ii.iccid, ii.devicestate, ii.devicemode, ii.devicesubmode, ii.interfacename)
+
+	cluster.shutdown() # Closes connection to the DB and frees resources.
+
+	print "DUMP FINISHED.\n"
+
